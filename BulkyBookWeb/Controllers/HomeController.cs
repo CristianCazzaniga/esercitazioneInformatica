244e0f8271de26be0b7b609ad5ee51c478102394
Cristian Cazzaniga <cristian.cazzaniga@issgreppi.it>
2023-01-19 11:39:07 +0100

    prima commit

--- BulkyBookWeb/Controllers/HomeController.cs
+++ BulkyBookWeb/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using BulkyBookWeb.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using System.Diagnostics;
 
 namespace BulkyBookWeb.Controllers;
--- BulkyBookWeb/Models/Category.cs
+++ BulkyBookWeb/Models/Category.cs
@@ -1,4 +1,6 @@
 ﻿
+using System;
+
 namespace BulkyBookWeb.Models;
 public class Category
 {
