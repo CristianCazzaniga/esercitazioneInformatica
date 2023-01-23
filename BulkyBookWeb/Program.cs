0637bc6ee6daf3c81fb275464e1cc851338b75d9
Cristian Cazzaniga <cristian.cazzaniga@issgreppi.it>
2023-01-23 13:48:53 +0100

    setup progrma.cs to use dbContext

--- BulkyBookWeb/Program.cs
+++ BulkyBookWeb/Program.cs
@@ -1,5 +1,8 @@
 
+using BulkyBookWeb.Data;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -7,7 +10,9 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(
+    builder.Configuration.GetConnectionString("DefaultConnection")
+    ));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
