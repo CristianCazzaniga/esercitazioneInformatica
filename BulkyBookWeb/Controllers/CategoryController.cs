7966903b3c00252aab070b5d4acc2225c462413e
Cristian Cazzaniga <cristian.cazzaniga@issgreppi.it>
2023-01-24 10:56:49 +0100

    Creazione del controller Category e aggiunta la view del controller

---
+++ BulkyBookWeb/Controllers/CategoryController.cs
@@ -0,0 +1,21 @@
+﻿using BulkyBookWeb.Data;
+using BulkyBookWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace BulkyBookWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Category> objCategoryList=_db.Categories;
+            return View(objCategoryList);
+        }
+    }
+}
--- BulkyBookWeb/Models/Category.cs
+++ BulkyBookWeb/Models/Category.cs
@@ -8,6 +8,7 @@ public class Category
     [Key]
     public int Id { get; set; }
     [Required]
+    [Display(Name="Nome Categoria")]
     public string Name { get; set; }
     public int DisplayOrder { get; set; }
     public DateTime CreatedDateTime { get; set; } = DateTime.Now;
