ab4cfc6391e39f6bc2c692c7bab1fdd797802848
cristian cazzaniga <cristian.cazzaniga@issgreppi.it>
2023-02-08 11:42:49 +0100

    Aggiunta del metodo delete e view associata

--- BulkyBookWeb/Controllers/CategoryController.cs
+++ BulkyBookWeb/Controllers/CategoryController.cs
@@ -70,5 +70,32 @@ namespace BulkyBookWeb.Controllers
             }
             return View(obj);
         }
+        
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var categoryFromDb = _db.Categories.Find(id);
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePost(int id, [Bind("Id")] Category obj)
+        {
+            if (id != obj.Id)
+            {
+                return NotFound();
+            }
+            _db.Categories.Remove(obj);
+            _db.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
     }
 }
