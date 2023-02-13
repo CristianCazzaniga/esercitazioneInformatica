f377e394df011cc8b732be0d2c3fadde643aa9d9
cristian cazzaniga <cristian.cazzaniga@issgreppi.it>
2023-02-13 10:22:00 +0100

    Aggiunto supporto a tempdata

--- BulkyBookWeb/Controllers/CategoryController.cs
+++ BulkyBookWeb/Controllers/CategoryController.cs
@@ -35,6 +35,7 @@ namespace BulkyBookWeb.Controllers
             {
                 _db.Categories.Add(obj);
                 _db.SaveChanges();
+                TempData["success"] = "Category created successfully";
                 return RedirectToAction(nameof(Index));
             }
             return View(obj);
@@ -66,6 +67,7 @@ namespace BulkyBookWeb.Controllers
             {
                 _db.Categories.Update(obj);
                 _db.SaveChanges();
+                TempData["success"] = "Category updated successfully";
                 return RedirectToAction(nameof(Index));
             }
             return View(obj);
@@ -95,6 +97,7 @@ namespace BulkyBookWeb.Controllers
             }
             _db.Categories.Remove(obj);
             _db.SaveChanges();
+            TempData["success"] = "Category deleted successfully";
             return RedirectToAction(nameof(Index));
         }
     }
