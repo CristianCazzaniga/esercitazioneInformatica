f3bf74244e380d443c7a5f686fb901d2f27b5b71
cristian cazzaniga <cristian.cazzaniga@issgreppi.it>
2023-02-20 10:59:22 +0100

    Implementate IunitOfWork e UnitOfWork

---
+++ BulkyBook.DataAccess/Repository/IRepository/UnitOfWork.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BulkyBook.DataAccess.Repository.IRepository
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly ApplicationDbContext _db;
+        public UnitOfWork(ApplicationDbContext db)
+        {
+            _db = db;
+            Category = new CategoryRepository(_db);
+        }
+        public ICategoryRepository Category { get; private set; } = null!;
+        public void Save()
+        {
+            _db.SaveChanges();
+        }
+    }
+}
