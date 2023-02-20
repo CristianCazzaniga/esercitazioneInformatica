df15311a92af46883c19e0ae70eb242fde05fa10
cristian cazzaniga <cristian.cazzaniga@issgreppi.it>
2023-02-20 11:55:07 +0100

    Aggiunta CoverTypeController

---
+++ BulkyBookWeb/Areas/Admin/Controllers/CoverTypeController.cs
@@ -0,0 +1,100 @@
+﻿using BulkyBook.DataAccess.Repository.IRepository;
+using BulkyBook.Models;
+using BulkyBook.Models.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace BulkyBookWeb.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CoverTypeController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public CoverTypeController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        //GET
+        public IActionResult Index()
+        {
+            IEnumerable<CoverType> objCoverTypeList = _unitOfWork.CoverType.GetAll();
+            return View(objCoverTypeList);
+        }
+        //GET
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(CoverType obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.CoverType.Add(obj);
+                TempData["success"] = "CoverType created successfully";
+                return RedirectToAction(nameof(Index));
+            }
+            return View(obj);
+        }
+        //GET
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var coverTypeFromDbFirst = _unitOfWork.CoverType.GetFirstOrDefault(u => u.Id == id);
+            if (coverTypeFromDbFirst == null)
+            {
+                return NotFound();
+            }
+            return View(coverTypeFromDbFirst);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(CoverType obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.CoverType.Update(obj);
+                _unitOfWork.Save();
+                TempData["success"] = "CoverType updated successfully";
+                return RedirectToAction(nameof(Index));
+            }
+            return View(obj);
+        }
+        //GET
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var coverTypeFromDbFirst = _unitOfWork.CoverType.GetFirstOrDefault(u => u.Id == id);
+            if (coverTypeFromDbFirst == null)
+            {
+                return NotFound();
+            }
+            return View(coverTypeFromDbFirst);
+        }
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePost(int id, [Bind("Id")] CoverType coverType)
+        {
+            if (id != coverType.Id)
+            {
+                return NotFound();
+            }
+            var coverTypeFromDbFirst = _unitOfWork.CoverType.GetFirstOrDefault(u => u.Id == id);
+            if (coverTypeFromDbFirst == null)
+            {
+                return NotFound();
+            }
+            _unitOfWork.CoverType.Remove(coverTypeFromDbFirst);
+            _unitOfWork.Save();
+            TempData["success"] = "CoverType deleted successfully";
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
