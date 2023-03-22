5b6b28011327b8ca6530cde8ba7cb9819145d3ef
cristian cazzaniga <cristian.cazzaniga@issgreppi.it>
2023-03-22 11:36:01 +0100

    Aggiunta Company Controller, Company js, create view associate, modificato dropdown list nel layout

---
+++ BulkyBookWeb/Areas/Admin/Controllers/CompanyController.cs
@@ -0,0 +1,95 @@
+﻿using BulkyBook.DataAccess.Repository.IRepository;
+using BulkyBook.Models;
+using BulkyBook.Models.Models;
+using BulkyBook.Models.ViewModels;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace BulkyBookWeb.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CompanyController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public CompanyController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        //GET
+        public IActionResult Index()
+        {
+            return View();
+        }
+        //GET
+        public IActionResult Upsert(int? id)
+        {
+            Company company = new();
+            if (id == null || id == 0)
+            {
+                //restituisce una view per la creazione di una nuova Company
+                return View(company);
+            }
+            else
+            {
+                var companyInDb = _unitOfWork.Company.GetFirstOrDefault(u => u.Id == id);
+                if (companyInDb != null)
+                {
+                    return View(company);
+                }
+                //la Company con l'id inviato non è stato trovata nel database.
+                //restituisce una view per creare una nuova Company
+                return View(company);
+            }
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Upsert(Company obj)
+        {
+            if (ModelState.IsValid)
+            {
+                if (obj.Id == 0)//new Company
+                {
+                    _unitOfWork.Company.Add(obj);
+                    TempData["success"] = "Company created successfully";
+                }
+                else //update exsisting Company
+                {
+                    _unitOfWork.Company.Update(obj);
+                    TempData["success"] = "Company updated successfully";
+                }
+                _unitOfWork.Save();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(obj);
+        }
+        #region API CALLS
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var companyList = _unitOfWork.Company.GetAll();
+            return Json(new { data = companyList });
+        }
+        [HttpDelete]
+        public IActionResult Delete(int? id)
+        {
+            var objFromDbFirst = _unitOfWork.Company.GetFirstOrDefault(u => u.Id == id);
+            if (objFromDbFirst == null)//l'oggetto con l'id specificato non è stato trovato
+            {
+                return Json(new { success = false, message = "Error while deleting" });
+            }
+            else //l'oggetto con l'id specificato è stato trovato
+            {
+                _unitOfWork.Company.Remove(objFromDbFirst);
+                _unitOfWork.Save();
+                return Json(new { success = true, message = "Delete Successful" });
+            }
+        }
+        #endregion
+    }
+}
